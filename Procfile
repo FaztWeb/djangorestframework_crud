041a285cb0c6ed65f29539885cc2ec5bea9367fc
fazt <fazt@faztweb.com>
2022-08-28 01:04:35 -0500

    deploy settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'drfsimplecrud.wsgi'
--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-j=jvx5r3z9yj$@s)3p0+uhz=8uoxr!83j6p+a#@^m$%(p5gm#b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = 'static/'
 
