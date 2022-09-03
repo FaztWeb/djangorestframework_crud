8ffd429042cbb1cb68ca94f2dce7c3a85e832fdd
Fazt <fazt@faztweb.com>
2022-09-03 12:33:35 -0500

    slash removed

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 
-echo $RENDER
-
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
