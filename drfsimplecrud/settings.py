65d98c137e09df81db4bf41658323c0f869d7330
Fazt <fazt@faztweb.com>
2022-09-03 11:54:07 -0500

    update installed apps

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects',
     'rest_framework',
+    'projects',
 ]
 
 MIDDLEWARE = [
